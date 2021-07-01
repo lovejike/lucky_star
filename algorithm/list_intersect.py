1c8fd43a6e3a602ac1f0aef3d454e6f7db9955a6
xuebingfang <xuebingfang@jd.com>
2021-07-01 10:41:44 +0800

    update

--- algorithm/list_intersect.py
+++ algorithm/list_intersect.py
@@ -19,36 +19,25 @@ class ListNode:
 
 
 class Solution:
-    def get_intersect_node(self, list_a, list_b):
-        head_a = list_a
-        head_b = list_b
-        len_a, len_b = 0, 0
-        while head_a:
-            head_a = head_a.next
-            len_a += 1
-        while head_b:
-            head_b = head_b.next
-            len_b += 1
-        head_a = list_a
-        head_b = list_b
-        if len_a > len_b:
-            for i in range(len_a - len_b):
-                head_a = head_a.next
-        elif len_b > len_a:
-            for i in range(len_b - len_a):
-                head_b = head_b.next
-        while head_a != head_b:
-            head_a = head_a.next
-            head_b = head_b.next
-        return head_a.val
+    def get_intersect_node(self, headA, headB) :
+        nodeA = headA
+        nodeB = headB
+        while nodeA != nodeB:
+            nodeA = nodeA.next if nodeA else headB
+            nodeB = nodeB.next if nodeB else headA
+        if nodeA:
+            return True
+        else:
+            return False
 
 
 if __name__ == "__main__":
     ln1 = ListNode(1)
-    ln2 = ListNode(3)
-    ln3 = ListNode(4)
-    ln1.next = ln3
-    ln2.next = ln3
+    ln2 = ListNode(2)
+    ln3 = ListNode(3)
+    ln4 = ListNode(4)
+    ln1.next = ln2
+    ln3.next = ln4
     s = Solution()
-    print(s.get_intersect_node(ln1, ln2))
+    print(s.get_intersect_node(ln1, ln3))
 
