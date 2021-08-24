6dba5db8cb46bb09b9151ff9d45a8bb727cb37a1
xuebingfang <xuebingfang@jd.com>
2021-08-24 18:23:00 +0800

    update

--- algorithm/get_max.py
+++ algorithm/get_max.py
@@ -1,26 +1,18 @@
-# 计算子数组的最大和
-def max_sum_func(in_list):
-    max_sum = 0
-    cur_sum = 0
-    if in_list is None:
-        return max_sum
+def maxSubArray(self, nums):
+    size = len(nums)
+    if size == 0:
+        return None
+    if size == 1:
+        return nums[0]
 
-    for value in in_list:
-        cur_sum += value
-        if cur_sum < 0:
-            cur_sum = 0
-        if cur_sum > max_sum:
-            max_sum = cur_sum
+    dp = [False for _ in range(size)]  # 记录状态：当前以i结尾的最大值记录在里面
+    dp[0] = nums[0]
 
-    if max_sum == 0:
-        max_sum = in_list[0]
-        for i in in_list[1:]:
-            if i > max_sum:
-                max_sum = i
-
-    return max_sum
+    for i in range(1, size):
+        dp[i] = max(dp[i - 1] + nums[i], nums[i])  # 记录最大状态
+    return max(dp)
 
 
 if __name__== '__main__':
     values = [1, -2, 3, 10, -4, 7, 2, -5]
-    print(max_sum_func(values))
+    print(maxSubArray(values))
