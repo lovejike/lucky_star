b6c89ffd8f94439d0bbb3c2d5da69ead0bf74dcd
xuebingfang <xuebingfang@jd.com>
2021-09-09 09:39:06 +0800

    update

--- algorithm/math.py
+++ algorithm/math.py
@@ -9,9 +9,34 @@ class Solution(object):
         for num in nums:
             res ^= num
         return res
+
+    # 二进制中1的个数
     def numsof1(self, n):
         count = 0
         while n&0xffffffff !=0:
             count += 1
             n = n & (n-1)
         return count
+
+    # 返回只出现一次的数字
+    def find_nums_appear_one(self, array):
+        if not array:
+            return
+        dict = {}
+        for i in array:
+            if i not in dict:
+                dict[i] = 1
+            else:
+                dict[i] += 1
+        # print(dict)
+        res = []
+        for i in dict:
+            if dict[i] == 1:
+                res.append(i)
+        return res
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.numsof1(7))
+    array = [2,3,4,4]
+    print(s.find_nums_appear_one(array))
