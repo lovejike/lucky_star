47196c07e8c048d5840678348d9a2fcdab9e9878
lovejike <2471701472@qq.com>
2022-06-05 17:04:55 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,4 +1,5 @@
 # 1. 绳子覆盖最多的点数 
+# planA,O(N*logN)
 def max_points(arr, L):   
   ans = 1   
   for i in range(len(arr)):     
@@ -16,4 +17,16 @@ def nearest_index(arr, R, value):
       R = mid - 1    
     else:       
       L = mid + 1  
-  return index             
+  return index 
+
+# planB 滑动窗口
+def max_point_2(arr, L):
+  left, right = 0, 0
+  n = len(arr)
+  max_ = 0
+  while L < n:
+    while right < n and arr[right]-arr[left] <= L:
+      right += 1
+    max_ = max(max_, right-left)
+    left += 1
+  return max_
